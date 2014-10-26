f77efe7c685b834e820d7f4f6de31ca4694a3222
Rafi Witten <rafiwitten@gmail.com>
2014-10-26 15:18:25 -0700

    Adjusted treatment of FCS and probability function.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import scipy
 from scipy import optimize
 import sys
 
-TOL = .01
+TOL = .001
 NUM_CALLED = 0
 
 def loadRawGames(filename):
@@ -59,20 +59,23 @@ def scoreGamesWithPowers(games, powers):
 	score = 0
 
 	GAME_SCALE = 1e1
-	POWER_SCALE = 1
+	POWER_SCALE = 1e-2
 	for g in games:
 		unitDiff = (powers[g["Winner/Tie"]] - powers[g["Loser/Tie"]])/2
-		ss = (GAME_SCALE*unitDiff) + .5
+#		ss = (GAME_SCALE*unitDiff) + .5
 
-		happiness = None
-		if ss >= 1.0:
-			happiness = 0
-		elif ss <= 0.01:
-			happiness = -10**9
-		else:
-			happiness = math.log(ss)
+#		happiness = None
+#		if ss >= 1.0:
+#			happiness = 0
+#		elif ss <= 0.01:
+#			happiness = -10**9
+#		else:
+#			happiness = math.log(ss)
+#		score += - (happiness)
 
-		score += - (happiness)
+		sigmHappiness = math.log(1.0 / (1.0 + math.exp(-100 * unitDiff)))
+
+		score += -(sigmHappiness)
 
 	for power in powers:
 		score += POWER_SCALE * power # don't give excess power than is needed.
@@ -84,10 +87,9 @@ def scoreGamesWithPowers(games, powers):
 
 	return score
 
-def assignPowers(scoreFunc, numberTeams):
-	powers = [.5 for i in range(numberTeams)]
-	bounds = [(0,1) for i in range(numberTeams)]
-	results = scipy.optimize.minimize(scoreFunc, powers, bounds = bounds, method = "SLSQP", tol = TOL)
+def assignPowers(scoreFunc, powerBounds):
+	powers = [.5 for i in range(len(powerBounds))]
+	results = scipy.optimize.minimize(scoreFunc, powers, bounds = powerBounds, method = "SLSQP", tol = TOL)
 	return results.x
 
 def prettyPrint(chosenPowers, teams, outputFile):
@@ -102,11 +104,32 @@ def prettyPrint(chosenPowers, teams, outputFile):
 		row = rows[i]
 		fh.write("[%d] %s (%.3f)\n" % (i+1, row[1], row[0]+1e-8))
 
+def boundPowers(games, teams):
+	numGames = [0 for i in range(len(teams))]
+
+	for g in games:
+		numGames[g["Winner/Tie"]] += 1
+		numGames[g["Loser/Tie"]] += 1
+
+	bounds = []
+	for g in numGames:
+		if g <= 2: # TEAM IS FCS, power is set to .5
+			bounds.append((.5,.5))
+		else:
+			bounds.append((0,1))
+
+#	for i in range(len(teams)):
+#		print("Team %s, bound %s" % (teams[i], bounds[i]))
+	return bounds
+
 def main():
 	games, teams = loadGames(sys.argv[1])
+
+	powerBounds = boundPowers(games, teams)
+
 	scorePowersFunc = lambda powers : scoreGamesWithPowers(games, powers)
-	chosenPowers = assignPowers(scorePowersFunc, len(teams))
-	prettyPrint(chosenPowers, teams,sys.argv[2])
+	chosenPowers = assignPowers(scorePowersFunc, powerBounds)
+	prettyPrint(chosenPowers, teams, sys.argv[2])
 
 if __name__ == "__main__":
 	main()
